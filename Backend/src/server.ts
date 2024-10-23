b8f89db877435d002a4b9e121c936747758eef91
artur-seppa <artur.reiman@gmail.com>
2024-10-23 20:19:48 -0300

    definindo cors

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import { router } from "./routes";
+import cors from "cors"
 
 const app = express();
 
+/* permite o acesso livre da requisicao do backend
+   sem precisar definir uma url especifica que a 
+   requisita
+*/
+app.use(cors());
 // configura como padrao o recebimento de dados via json
 app.use(express.json());
 //obtem as rotas definidas
 app.use(router);
 
-app.listen(3333, () => 
+app.listen(3333, () =>
     console.log("server is running on http://localhost:3333")
 );
 
